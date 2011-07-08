b4783521f221ee2b29125f307962f0ea23497ed9
Claude Rogers <claude.j.rogers@gmail.com>
2011-07-07 20:36:45 -0700

    Initial Commit

---
+++ edge_detect/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  edge_detect
+//
+//  Created by Claude Rogers on 7/7/11.
+//  Copyright 2011 California Institute of Technology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
